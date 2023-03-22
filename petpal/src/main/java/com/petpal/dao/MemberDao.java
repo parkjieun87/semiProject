3769ebbf8e45bcbc1c1f9b0dd223117cab8b7943
HSYOON27 <kots524@naver.com>
2023-03-22 14:39:11 +0900

    회원탈퇴 기능 아이디 찾기 기능 구현

--- petpal/src/main/java/com/petpal/dao/MemberDao.java
+++ petpal/src/main/java/com/petpal/dao/MemberDao.java
@@ -37,6 +37,7 @@ public class MemberDao {
 		};
 		jdbcTemplate.update(sql, param);			
 		}
+	
 	//조회를 위한 RowMapper 구현 (형석)
 		private RowMapper<MemberDto> mapper = new RowMapper<MemberDto>() {
 			@Override
@@ -72,6 +73,32 @@ public class MemberDao {
 			return jdbcTemplate.update(sql, param) > 0 ; //update() 메소드의 반환값이 0보다 크면 true를 반환하고, 그렇지 않으면 false를 반환.
 			//이는 데이터베이스에서 비밀번호 변경 작업이 성공했는지의 여부를 나타낸다.
 		}
+		
+	// 비밀번호 제외한 나머지 정보 변경 기능 구현(형석)
+		public boolean changeInformation(MemberDto memberDto) {
+			String sql = "update member set member_email=?, member_tel=?, member_nick=?, "
+					+ "member_post=?, member_basic_addr=?, member_detail_addr=?"
+					+ "where member_id=?";
+			
+			Object[] param = {
+				memberDto.getMemberEmail(), memberDto.getMemberTel(), memberDto.getMemberNick(),
+				memberDto.getMemberPost(), memberDto.getMemberBasicAddr(), memberDto.getMemberDetailAddr(),
+			};
+			return jdbcTemplate.update(sql, param) > 0;//update() 메소드의 반환값이 0보다 크면 true를 반환하고, 그렇지 않으면 false를 반환.
+			//이는 데이터베이스에서 비밀번호 변경 작업이 성공했는지의 여부를 나타낸다.	
+		}
+		
+	//	회원 탈퇴 기능 (형석)
+		public boolean delete(String memberId) {
+			String sql = "delete member where member_id = ?";
+			Object[] param = {memberId};
+			return jdbcTemplate.update(sql, param) > 0 ;
+		}
+		
+	//	아이디 찾기 기능(닉네임, 전화번호로)
+//		public String findId(MemberDto memberDto) {
+//			
+//		}
 		
 		
 	}
