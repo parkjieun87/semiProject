c73f0950a97d13f5d6439e5241c83e9ac55e2f4f
tomisoulja <lsh392766@gmail.com>
2023-03-22 14:38:34 +0900

    관리자 컨트롤러 생성

---
+++ petpal/src/main/java/com/petpal/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.petpal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.petpal.dao.MemberDao;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+	@Autowired MemberDao memberdao;
+}
