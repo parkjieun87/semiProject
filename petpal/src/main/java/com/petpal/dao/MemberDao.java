334c0334056f55648b9f3f7584f9ec522cce28e8
jaeyoung1375 <jaeyoung1375@naver.com>
2023-03-22 17:49:59 +0900

    휴대폰 인증 구현

--- petpal/src/main/java/com/petpal/dao/MemberDao.java
+++ petpal/src/main/java/com/petpal/dao/MemberDao.java
@@ -2,14 +2,19 @@ package com.petpal.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.petpal.dto.MemberDto;
+
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
 
 @Repository
 public class MemberDao {
@@ -18,25 +23,25 @@ public class MemberDao {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
-	public void insert(MemberDto memberDto) {
-		String sql = "insert into member("
-				+ "member_id, member_pw, member_name"
-				+ "member_email, member_tel, member_nick"
-				+ "adminck, member_regdate, member_post"
-				+ "member_basic_addr, member_detail_addr"
-				+ ")value("
-				+ "?, ?, ?, ?, ?, ?, 0, sysdate, ?, ?, ?"
-				+ ")";
-		Object[] param = {
-				memberDto.getMemberId(), memberDto.getMemberPw(),
-				memberDto.getMemberName(), memberDto.getMemberEmail(),
-				memberDto.getMemberTel(), memberDto.getMemberNick(),
-				memberDto.getAdminCk(), memberDto.getMemberRegdate(),
-				memberDto.getMemberPost(), memberDto.getMemberBasicAddr(),
-				memberDto.getMemberDetailAddr()
-		};
-		jdbcTemplate.update(sql, param);			
-		}
+	   public void insert(MemberDto memberDto) {
+		      String sql = "insert into member("
+		            + "member_id, member_pw, member_name, "
+		            + "member_email, member_tel, member_nick ,"
+		            + "adminck, member_regdate, member_post, "
+		            + "member_basic_addr, member_detail_addr"
+		            + ")values("
+		            + "?, ?, ?, ?, ?, ?, 0, sysdate, ?, ?, ?"
+		            + ")";
+		      Object[] param = {
+		            memberDto.getMemberId(), memberDto.getMemberPw(),
+		            memberDto.getMemberName(), memberDto.getMemberEmail(),
+		            memberDto.getMemberTel(), memberDto.getMemberNick(),
+		            memberDto.getMemberPost(), memberDto.getMemberBasicAddr(),
+		            memberDto.getMemberDetailAddr()
+		      };
+		      jdbcTemplate.update(sql, param);         
+		      }
+
 	
 	//조회를 위한 RowMapper 구현 (형석)
 		private RowMapper<MemberDto> mapper = new RowMapper<MemberDto>() {
@@ -112,6 +117,32 @@ public class MemberDao {
 			return jdbcTemplate.update(sql, param) > 0;
 			
 		}
+		
+		public void certifiedPhoneNumber(String userPhoneNumber, int randomNumber) {
+			String api_key = "NCS2MIOLDQAQDKUM";
+		    String api_secret = "L9BVTDO86DNWMOLVZNOHKM7CB9WHFLIA";
+		    Message coolsms = new Message(api_key, api_secret);
+
+		    // 4 params(to, from, type, text) are mandatory. must be filled
+		    HashMap<String, String> params = new HashMap<String, String>();
+		    params.put("to", "01088191375");    // 수신전화번호
+		    params.put("from", "01088191375");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+		    params.put("type", "SMS");
+		    params.put("text", "[TEST] 인증번호는" + "["+randomNumber+"]" + "입니다."); // 문자 내용 입력
+		    params.put("app_version", "test app 1.2"); // application name and version
+
+		    try {
+		        JSONObject obj = (JSONObject) coolsms.send(params);
+		        System.out.println(obj.toString());
+		      } catch (CoolsmsException e) {
+		        System.out.println(e.getMessage());
+		        System.out.println(e.getCode());
+		      }
+		    
+		}
+		
+		
+		
 		
 		
 	}
