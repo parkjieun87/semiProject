fd6443387e951ae3d1d89ece5fc883fd041002d2
tomisoulja <lsh392766@gmail.com>
2023-03-31 17:48:04 +0900

    SalesDto 수정

--- petpal/src/main/java/com/petpal/dto/SalesDto.java
+++ petpal/src/main/java/com/petpal/dto/SalesDto.java
@@ -10,18 +10,5 @@ import lombok.Data;
 public class SalesDto {
 	
 	private Date orderDate;
-	private int productCount;
-	private int productPrice;
 	private long total;
-	private String receiverName;
-	private String receiverBasicAddr;
-	private String receiverPost;
-	private String receiverDetailAddr;
-	private String receiverTel;
-	private String productName;
-	
-	
-	
-	
-	
 }
