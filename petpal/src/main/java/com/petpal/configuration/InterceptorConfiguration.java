dad3d66b31fe0ff27d5a256754aa79f7b3ed2e35
[dksxlzhd0807] <[dksxlzhd0807@naver.com]>
2023-04-05 13:27:40 +0900

    order

--- petpal/src/main/java/com/petpal/configuration/InterceptorConfiguration.java
+++ petpal/src/main/java/com/petpal/configuration/InterceptorConfiguration.java
@@ -2,9 +2,11 @@ package com.petpal.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 import com.petpal.interceptor.AdminInterceptor;
 import com.petpal.interceptor.MemberInterceptor;
+import com.petpal.interceptor.OrderInterceptor;
 import com.petpal.interceptor.TestInterceptor;
 
 @Configuration
@@ -19,5 +21,29 @@ public class InterceptorConfiguration {
 	@Autowired
 	private AdminInterceptor adminInterceptor;
 	
+	@Autowired
+	private OrderInterceptor orderInterceptor;
 	
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//		//인터셉터 등록 메소드
+//				//- 매개변수에 있는 registry를 사용하여 원하는 인터셉터를 원하는 주소에 설정
+//				
+//				//주소 패턴 설정
+//				//- Spring표현식 사용
+//				//- *가 1개면 해당 엔드포인트의 모든 내용을 의미
+//				//- *가 2개면 해당 엔드포인트와 그 이하의 모든 내용을 의미
+//				
+//				//[1] TestInterceptor를 모든 주소에 설정하겠다!
+//				//registry.addInterceptor(testInterceptor)
+//				//			.addPathPatterns("/**");
+//							
+//				//[2] MemberInterceptor를 다음 페이지에 설정하겠다!
+//				//- /member로 시작하는 주소 중에서 비회원 페이지 제거
+//				//- /admin으로 시작하는 주소 전체
+//		
+//		registry.addInterceptor(InterceptorRegistry registry){
+//			
+//		
+//	}
 }
