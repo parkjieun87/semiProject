a5d0edba7ea36b03d39c36b7f27c7cee5881b1a4
HSYOON27 <kots524@naver.com>
2023-04-06 12:11:03 +0900

    dd

--- petpal/src/main/java/com/petpal/dao/MemberDao.java
+++ petpal/src/main/java/com/petpal/dao/MemberDao.java
@@ -193,14 +193,14 @@ public class MemberDao {
       
       // 번호 인증 기능
       public void certifiedPhoneNumber(String userPhoneNumber, int randomNumber) {
-         String api_key = "NCSZ1CBJD1Z9UFVN";
-          String api_secret = "VUHE9YGFBNRPUPKWK2REZWOGOXJITDES";
+         String api_key = "NCSDBELKOW4C91N8";
+          String api_secret = "CU99NCUZXCJZP6JALSSY8YYHSFVZFTL9";
           Message coolsms = new Message(api_key, api_secret);
 
           // 4 params(to, from, type, text) are mandatory. must be filled
           HashMap<String, String> params = new HashMap<String, String>();
           params.put("to", userPhoneNumber);    // 수신전화번호
-          params.put("from", "01056300144");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+          params.put("from", "01053261408");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
           params.put("type", "SMS");
           params.put("text", "[TEST] 인증번호는" + "["+randomNumber+"]" + "입니다."); // 문자 내용 입력
           params.put("app_version", "test app 1.2"); // application name and version
