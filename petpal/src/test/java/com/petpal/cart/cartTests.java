4041b4e0149f9a43eff026da9e259865464812e1
parkjieun87 <dksxlzhd0807@naver.com>
2023-06-30 14:59:26 +0900

    커밋

--- petpal/src/test/java/com/petpal/cart/cartTests.java
+++ petpal/src/test/java/com/petpal/cart/cartTests.java
@@ -1,102 +1,102 @@
-package com.petpal.cart;
-
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.petpal.dao.CartDao;
-import com.petpal.dto.CartDto;
-import com.petpal.service.CartService;
-
-@SpringBootTest
-public class cartTests {
-	
-	@Autowired
-	private CartDao cartDao;
-	
-	@Autowired
-	private CartService cartService;
-	
+//package com.petpal.cart;
+//
+//import java.util.List;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import com.petpal.dao.CartDao;
+//import com.petpal.dto.CartDto;
+//import com.petpal.service.CartService;
+//
+//@SpringBootTest
+//public class cartTests {
+//	
+//	@Autowired
+//	private CartDao cartDao;
+//	
+//	@Autowired
+//	private CartService cartService;
+//	
+////	@Test
+////	public void addCart() {
+////		String memberId = "testuser1";
+////		int productNo = 2;
+////		int count = 2;
+////		
+////		CartDto cart = new CartDto();
+////		cart.setMemberId(memberId);
+////		cart.setProductNo(productNo);
+////		cart.setProductCount(count);
+////		
+////		int result = 0;
+////		result = cartDao.addCart(cart);
+////		
+////		//System.out.println("결과 : "+result);
+////	}
+//	
+////	@Test
+////	public void deleteCart() {
+////		int cartNo = 21;
+////		cartDao.deleteCart(cartNo);
+////		
+////	}
+//	
+////	@Test
+////	public void modifyCartTest() {
+////		int cartNo = 23;
+////		int count = 10;
+////		
+////		CartDto cart = new CartDto();
+////		cart.setCartNo(cartNo);
+////		cart.setProductCount(count);
+////		
+////		cartDao.modifyCount(cart);
+////	}
+//	
+////	@Test
+////	public void getCartTest() {
+////		String memberId = "testuser1";
+////		
+////		List<CartDto> list = cartDao.getCart(memberId);
+////		for(CartDto cart : list) {
+////			//System.out.println(cart);
+////			cart.initSaleTotal();
+////			//System.out.println("init cart : "+cart);
+////		}
+////	}
+//	
+////	@Test
+////	public void checkCartTest() {
+////		String memberId = "testuser1";
+////		int productNo = 2;
+////		
+////		CartDto cart = new CartDto();
+////		cart.setMemberId(memberId);
+////		cart.setProductNo(productNo);
+////		
+////		CartDto resultCart = cartDao.checkCart(cart);
+////		//System.out.println("결과 : "+resultCart);
+////	}
+//	
 //	@Test
-//	public void addCart() {
-//		String memberId = "testuser1";
+//	public void addCartTest() {
+//		
+//		// given
+//		String memberId = "testuser21121";
 //		int productNo = 2;
-//		int count = 2;
+//		int count = 11;
 //		
-//		CartDto cart = new CartDto();
-//		cart.setMemberId(memberId);
-//		cart.setProductNo(productNo);
-//		cart.setProductCount(count);
-//		
-//		int result = 0;
-//		result = cartDao.addCart(cart);
-//		
-//		//System.out.println("결과 : "+result);
+//		CartDto dto = new CartDto();
+//		dto.setMemberId(memberId);
+//		dto.setProductNo(productNo);
+//		dto.setProductCount(count);
+//		// when
+//		int result = cartService.addCart(dto);
+//		// then
+//		//System.out.println("result : "+result);
 //	}
-	
-//	@Test
-//	public void deleteCart() {
-//		int cartNo = 21;
-//		cartDao.deleteCart(cartNo);
-//		
-//	}
-	
-//	@Test
-//	public void modifyCartTest() {
-//		int cartNo = 23;
-//		int count = 10;
-//		
-//		CartDto cart = new CartDto();
-//		cart.setCartNo(cartNo);
-//		cart.setProductCount(count);
-//		
-//		cartDao.modifyCount(cart);
-//	}
-	
-//	@Test
-//	public void getCartTest() {
-//		String memberId = "testuser1";
-//		
-//		List<CartDto> list = cartDao.getCart(memberId);
-//		for(CartDto cart : list) {
-//			//System.out.println(cart);
-//			cart.initSaleTotal();
-//			//System.out.println("init cart : "+cart);
-//		}
-//	}
-	
-//	@Test
-//	public void checkCartTest() {
-//		String memberId = "testuser1";
-//		int productNo = 2;
-//		
-//		CartDto cart = new CartDto();
-//		cart.setMemberId(memberId);
-//		cart.setProductNo(productNo);
-//		
-//		CartDto resultCart = cartDao.checkCart(cart);
-//		//System.out.println("결과 : "+resultCart);
-//	}
-	
-	@Test
-	public void addCartTest() {
-		
-		// given
-		String memberId = "testuser21121";
-		int productNo = 2;
-		int count = 11;
-		
-		CartDto dto = new CartDto();
-		dto.setMemberId(memberId);
-		dto.setProductNo(productNo);
-		dto.setProductCount(count);
-		// when
-		int result = cartService.addCart(dto);
-		// then
-		//System.out.println("result : "+result);
-	}
-	
-
-}
+//	
+//
+//}
